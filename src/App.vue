4c950b15ea058dae2182007903ff76c0cf552f85
solaireh3 <68488928+solaireh3@users.noreply.github.com>
2025-07-04 15:00:14 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
   <footer>
     <span>© 2025 Made in <a href="/" target="_blank">{{ userName }}</a></span>
     <a v-if="icpNumber" href="https://icp.gov.moe/?keyword=20252110" target="_blank">{{ icpNumber }}</a>
+    <a v-if="policenumber" :href="`https://beian.mps.gov.cn/#/query/webSearch?police=${policenumber}`" target="_blank" class="police_link">
+      <span class="police_img"></span> {{ policenumber }}
     </a>
   </footer>
 </template>
